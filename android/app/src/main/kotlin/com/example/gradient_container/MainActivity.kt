aff3d5d2cb084889b4fc027e183cde600a1f9217
Hp <shrutisaini1415@gmail.com>
2023-06-22 02:21:35 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/gradient_container/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gradient_container
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  Color(0xff8561c5),
+                  Colors.purple.shade100
+                ],
+              begin: FractionalOffset(1.0,0.0),
+              end: FractionalOffset(0.0,1.0),
+            )
+          ),
+          width: 200,
+          height: 300,
+        ),
+      )
+    );
+  }
+}
